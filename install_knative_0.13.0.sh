d5e9dca75e3163b6b0970787381eb771798ab546
anthony <ianthony7@163.com>
2021-04-09 13:44:35 +0800

    update dockerfile

--- install_knative_0.13.0.sh
+++ install_knative_0.13.0.sh
@@ -1,11 +1,12 @@
 # Install the Knative Serving
 export KNATIVE_VERSION="0.13.0"
-kubectl apply --filename "https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-crds.yaml"
-kubectl apply --filename "https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-core.yaml"
+export KNATIVE_CAOZUP="delete"
+kubectl $KNATIVE_CAOZUP --filename "https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-crds.yaml"
+kubectl $KNATIVE_CAOZUP --filename "https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-core.yaml"
 
 # Configure the magic xip.io DNS name
-kubectl apply --filename "https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-default-domain.yaml"
+kubectl $KNATIVE_CAOZUP --filename "https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-default-domain.yaml"
 
 # Install and configure Kourier
-kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v$KNATIVE_VERSION/third_party/kourier-latest/kourier.yaml
+kubectl $KNATIVE_CAOZUP --filename https://raw.githubusercontent.com/knative/serving/v$KNATIVE_VERSION/third_party/kourier-latest/kourier.yaml
 kubectl patch configmap/config-network --namespace knative-serving --type merge --patch '{"data":{"ingress.class":"kourier.ingress.networking.knative.dev"}}'
